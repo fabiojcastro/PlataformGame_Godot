25df14b04c7f361c9fa267b30a5d2b3c79fd895c
Fabio Castro <fabio23.castro@gmail.com>
2022-08-14 21:28:57 -0300

    creating actions player

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -3,23 +3,40 @@ class_name Player
 
 # habilitar tipagem
 onready var player_sprite: Sprite = get_node("Texture")
-
+onready var wall_ray: RayCast2D = get_node("WallRay")
 #criar o vetor de direção
 var velocity: Vector2 #(x, y)
 
 #varíavel speed criada no template do player (barra direita godot)
-export(int) var speed
 
+var direction: int = 1
 #parte do pulo e queda
 var jump_count: int = 0
+
+var landing: bool = false
+var on_wall: bool = false
+var attacking: bool = false
+var defending: bool = false
+var crouching: bool = false
+
+#flag para saber se o jogador pode se movimentar ou não, se uma das acima for true o debaixo fica false.
+var can_track_input: bool = true
+var not_on_wall: bool = true
+
+export(int) var speed
 export(int) var jump_speed
 export(int) var player_gravity
+
+export(int) var wall_jump_speed
+export(int) var wall_gravity
+export(int) var wall_impulse_speed
 
 #objetos que utilizam fisica, metodo void
 #a segunda linha pega o valor armazenado no metodo debaixo e insere na função especifica de movimento e colisão GODOT.
 func _physics_process(delta: float): 
 	horizontal_movement_env()
 	vertical_movement_env()
+	actions_env()
 	
 	gravity(delta)
 	velocity = move_and_slide(velocity, Vector2.UP)
@@ -29,19 +46,71 @@ func _physics_process(delta: float):
 # nessa função ja capturei o input do teclado, se executar o player e pressionar as teclas veremos alteração no terminal
 func horizontal_movement_env() -> void:
 	var input_direction: float = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	if can_track_input == false or attacking:
+		velocity.x = 0
+		return
+		
 	velocity.x = input_direction * speed
 
 func vertical_movement_env() -> void:
-	if is_on_floor():
+	if is_on_floor() or is_on_wall():
 		jump_count = 0
-	if Input.is_action_just_pressed("ui_select") and jump_count < 2:
+	if Input.is_action_just_pressed("ui_select") and jump_count < 2 and can_track_input and not attacking:
 		jump_count += 1
-		velocity.y  = jump_speed
+		if next_to_wall() and not is_on_floor():
+			velocity.y = wall_jump_speed
+			velocity.x += wall_impulse_speed * direction
+		else:
+			velocity.y  = jump_speed
 	
+func actions_env() -> void:
+	attack()
+	crouch()
+	defense()
+	
+func attack() -> void:
+	var attack_condition: bool = not attacking and not defending and not crouching
+	if Input.is_action_just_pressed("attack") and attack_condition and is_on_floor():
+		attacking = true
+		player_sprite.normal_attack = true
+	
+func crouch() -> void:
+	if Input.is_action_pressed("crouch") and not defending and is_on_floor():
+		crouching = true
+		can_track_input = false
+	elif not defending:
+		can_track_input = true
+		crouching = false
+		player_sprite.crouching_off = true
+	
+func defense() -> void:
+	if Input.is_action_pressed("defense") and not crouching and is_on_floor():
+		defending = true
+		can_track_input = false
+	elif not crouching:
+		defending = false
+		can_track_input = true
+		player_sprite.shield_off = true
+
 func gravity(delta: float) -> void:
-	velocity.y += delta * player_gravity
-	if velocity.y >= player_gravity:
-		velocity.y = player_gravity
+	if next_to_wall():
+		velocity.y += wall_gravity * delta
+		if velocity.y >= wall_gravity:
+			velocity.y = wall_gravity
+	else:
+		velocity.y += delta * player_gravity
+		if velocity.y >= player_gravity:
+			velocity.y = player_gravity
 		
+func next_to_wall() -> bool:
+	if wall_ray.is_colliding() and not is_on_floor():
+		if not_on_wall:
+			velocity.y = 0
+			not_on_wall = false
+			
+		return true
+	else:
+		not_on_wall = true
+		return false
 		
-		
+	
--- scripts/player/player_texture.gd
+++ scripts/player/player_texture.gd
@@ -1,22 +1,62 @@
 extends Sprite
 class_name PlayerTexture
 
+var normal_attack: bool = false
+var suffix: String = "_right"
+var shield_off: bool = false
+var crouching_off: bool = false
+
 #Linha para integrar o nó Animation Player, isso é necessário pois o Animation Player é filho e não pai, impedindo usar a função get_node direto.
 export(NodePath) onready var animation = get_node(animation) as AnimationPlayer
-
+export(NodePath) onready var player = get_node(player) as KinematicBody2D
 #esta função foi criada no script do Player no pai.
 func animate(direction: Vector2) -> void:
 	verify_position(direction)
-	horizontal_behavior(direction)
+	
+	if player.attacking or player.defending or player.crouching or player.next_to_wall():
+		action_behavior()
+	elif direction.y != 0:
+		vertical_behavior(direction)
+	elif player.landing == true:
+		animation.play("landing")
+	else:
+		horizontal_behavior(direction)
 	
 #condição para fazer o personagem virar o sprite para a esquerda ou direita. 
 func verify_position(direction: Vector2) -> void:
 	if direction.x > 0:
 		flip_h = false
+		suffix = "_right"
+		position = Vector2.ZERO
+		player.direction = -1
+		player.wall_ray.cast_to = Vector2(5.5, 0)
 	elif direction.x < 0:
 		flip_h = true
+		suffix = "_left"
+		position = Vector2(-2,0)
+		player.direction = 1
+		player.wall_ray.cast_to = Vector2(-7.5, 0)
+
+func action_behavior() -> void:
+	if player.next_to_wall():
+		animation.play("wall_slide")
+	elif player.attacking and normal_attack:
+		animation.play("attack" + suffix)
+	elif player.defending and shield_off:
+		animation.play("shield")
+		shield_off = false
+	elif player.crouching and crouching_off:
+		animation.play("crouch")
+		crouching_off = false
 
 #Texture é irmão de textura
+func vertical_behavior(direction: Vector2) -> void:
+	if direction.y > 0:
+		player.landing = true
+		animation.play("fall")
+	elif direction.y < 0:
+		animation.play('jump') 
+
 func horizontal_behavior(direction: Vector2) -> void:
 	if direction.x != 0:
 		#play run
@@ -24,3 +64,19 @@ func horizontal_behavior(direction: Vector2) -> void:
 	else:
 		#idle run
 		animation.play("idle")
+
+
+func _on_animation_finished(anim_name: String):
+	match	anim_name:
+		"landing":
+			player.landing = false
+			player.set_physics_process(true)
+		"attack_left":
+			normal_attack = false
+			player.attacking = false
+		"attack_right":
+			normal_attack = false
+			player.attacking = false
+			
+			
+	pass # Replace with function body.
